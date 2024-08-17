49db7f0ecfcff0e386f737265b1915935d3ca5ac
“fincherc” <christian.fincher@gmail.com>
2024-08-17 12:11:22 -0400

    bubble chart made

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## References
 
-Huntyr Github Repo - https://github.com/Huntyr/Homework-15/blob/8869628ca7d16954ddae0881a39360d60a5e3c1c/static/js/app.js#L140-L143
+- Huntyr Github Repo: https://github.com/Huntyr/Homework-15/blob/8869628ca7d16954ddae0881a39360d60a5e3c1c/static/js/app.js#L140-L143
+
+- Bubble Graph: https://d3-graph-gallery.com/graph/bubble_basic.html
+- Marker Color: https://stackoverflow.com/questions/47113270/assign-colors-to-bubbles-based-on-percentage-value-coming-from-input
+- Market Text: https://stackoverflow.com/questions/25176936/d3js-selectall-text
--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -18,7 +18,7 @@ function buildMetadata(sample) {
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-    filteredMetadata.foreach(function(item) {
+    filteredMetadata.forEach(function(item) {
       panel.append('p')
         .text(`${item.key}: ${item.value}`);
     });
@@ -30,15 +30,49 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
+    let samples = data.samples;
 
 
     // Filter the samples for the object with the desired sample number
-
+    const filteredSamples = samples.filter(function(object) {
+      return object.id === sample;
+    });
 
     // Get the otu_ids, otu_labels, and sample_values
+    let otu_ids = filteredSamples.otu_ids;
+    let otu_labels = filteredSamples.otu_labels;
+    let sample_values = filteredSamples.sample_values;
 
 
     // Build a Bubble Chart
+    const svg = d3.select("#bubble")
+      .append("svg")
+      .attr("width", 800)
+      .attr("height", 400);
+
+    //marker colors
+    var color = d3.scaleThreshold().domain(otu_ids).range(["red", "yellow", "green", "blue"]);
+
+    svg.selectAll("circle")
+      .data(otu_ids)
+      .enter()
+      .append("circle")
+      // Use the otu_ids for the x values.
+      .attr("cx", (d,i) => d)
+      // Use the sample_values for the y values.
+      .attr("cy", (d,i) => sample_values[i])
+      // Use the sample_values for the marker size.
+      .attr("radius", (d,i) => sample_values[i])
+      // Use the otu_ids for the marker colors.
+      .attr("fill", (d) => color[d])
+
+    svg.selectAll("text")
+      .data(otu_labels)
+      .enter()
+      .append("text")
+      .attr("x", (d, i) => otu_ids[i]) // Position text based on x-coordinate of circles
+      .attr("y", (d, i) => sample_values[i]) // Position text based on y-coordinate of circles
+      .text((d) => d); // Set the text content to the otu_labels
 
 
     // Render the Bubble Chart
